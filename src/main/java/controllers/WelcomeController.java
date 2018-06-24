66bd6756e73188705de2af675f7a902ae9a79be4
onl_ont <onl.ont@mail.ru>
2018-06-24 10:57:16 +0300

    proc

--- src/main/java/controllers/WelcomeController.java
+++ src/main/java/controllers/WelcomeController.java
@@ -16,9 +16,20 @@ public class WelcomeController {
     public String send() {
         return "index";
     }
+
     @ResponseBody
     @RequestMapping(value = "/api/v1/user/create", method = RequestMethod.POST)
     public String reg(HttpServletRequest request) {
+        System.out.println(request);
+        return "{\n" +
+                "\"userId\": \"01-000000000000001\",\n" +
+                "\"token\": \"toaWaep4chou7ahkoogiu9Iusaht9ima\"\n" +
+                "}";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/api/v1/user/token", method = RequestMethod.POST)
+    public String token(HttpServletRequest request) {
         System.out.println(request);
         return "{\n" +
                 "\"userId\": \"01-000000000000001\",\n" +
