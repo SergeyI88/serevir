5b6a36087c67599b336c6fb473c4b40a31d35d90
onl_ont <onl.ont@mail.ru>
2018-06-24 10:12:37 +0300

    proc

--- src/main/java/controllers/WelcomeController.java
+++ src/main/java/controllers/WelcomeController.java
@@ -2,6 +2,9 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class WelcomeController {
@@ -9,6 +12,12 @@ public class WelcomeController {
 
     @RequestMapping("/")
     public String send() {
+        return "index";
+    }
+
+    @RequestMapping("/api/v1/user/create")
+    public String reg(HttpServletRequest request) {
+        System.out.println(request);
         return "index";
     }
 }
