3d19551f74a4244cff78db60ceb063cbe1fdbdd9
onl_ont <onl.ont@mail.ru>
2018-06-24 10:16:01 +0300

    proc

--- src/main/java/controllers/WelcomeController.java
+++ src/main/java/controllers/WelcomeController.java
@@ -2,7 +2,7 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.portlet.ModelAndView;
+
 
 import javax.servlet.http.HttpServletRequest;
 
