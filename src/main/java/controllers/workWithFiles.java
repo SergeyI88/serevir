222c65a908ed66ba58decb92e8c792742b0c500d
Русский <alexey.shalnoy@yandex.ru>
2018-06-27 21:24:35 +0300

    Выгрузка файла

--- src/main/java/controllers/workWithFiles.java
+++ src/main/java/controllers/workWithFiles.java
@@ -1,18 +1,14 @@
 package controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import utils.MediaTypeUtils;
-
 import javax.servlet.ServletContext;
-import java.io.File;
-import java.io.FileInputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URL;
 
 @Controller
 public class workWithFiles {
@@ -20,20 +16,26 @@ public class workWithFiles {
     @Autowired
     ServletContext servlerContext;
 
-    final String DIRECTORY = "src/main/webapp/resources/files";
     final String DEFAULT_FILE_NAME = "products.xlsx";
+    String urlString = "https://drive.google.com/uc?export=download&id=16uGeZvBL8Ttn4YBT-vo5GmUh1MHC_eXi";
 
-    @RequestMapping("/download")
-    public ResponseEntity<InputStreamResource> downloadFile(
-            @RequestParam(defaultValue = DEFAULT_FILE_NAME) String fileName
-    ) throws Exception{
-        MediaType mediaType = MediaTypeUtils.getMediType(this.servlerContext, DEFAULT_FILE_NAME);
-        File file = new File(DIRECTORY + "/" + DEFAULT_FILE_NAME);
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-        System.out.println(resource.toString());
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName())
-                .contentType(mediaType)
-                .contentLength(file.length())
-                .body(resource);
+    @GetMapping("/download")
+    public void downloadFile3(HttpServletResponse resonse,
+                              @RequestParam(defaultValue = DEFAULT_FILE_NAME) String fileName) throws IOException {
+
+
+        // Content-Disposition
+        resonse.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + "products.xlsx");
+
+        BufferedInputStream inStream = new BufferedInputStream(new URL(urlString).openStream());
+        BufferedOutputStream outStream = new BufferedOutputStream(resonse.getOutputStream());
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = 0;
+        while ((bytesRead = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+        outStream.flush();
+        inStream.close();
     }
 }
