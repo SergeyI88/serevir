bb10dd61d397f553bfeb1e3c47861051da584f5c
admin <onl.ont@mail.ru>
2018-06-28 13:51:14 +0300

    proc

--- src/main/java/controllers/ControllerEvents.java
+++ src/main/java/controllers/ControllerEvents.java
@@ -1,18 +1,14 @@
 package controllers;
 
+import com.google.gson.Gson;
+import controllers.json.EventInstallOrUninstall;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import service.ClientService;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Enumeration;
 
 @Controller
 public class ControllerEvents {
@@ -21,31 +17,35 @@ public class ControllerEvents {
     @Autowired
     private ClientService clientService;
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public String installedOrDeleted(HttpServletRequest request) {
-        Enumeration<String> enumerPar = request.getParameterNames();
-        while (enumerPar.hasMoreElements()) {
-            String param = enumerPar.nextElement();
-            logger.info(param);
-            logger.info(request.getParameter(param));
-        }
+    @RequestMapping(value = "/", method = RequestMethod.POST, consumes = {"application/json"})
+    public String installedOrDeleted(@RequestBody String body) {
+        Gson gson = new Gson();
+        EventInstallOrUninstall event = gson.fromJson(body, EventInstallOrUninstall.class);
+        logger.info(event);
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()))) {
-            String string = reader.readLine();
-            while (string != null) {
-                logger.info(string + " зашли");
-                string = reader.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Enumeration<String> enumer = request.getHeaderNames();
-        while (enumer.hasMoreElements()) {
-            String header = enumer.nextElement();
-            logger.info(header);
-            logger.info(request.getHeader(header));
-        }
+//        Enumeration<String> enumerPar = request.getParameterNames();
+//        while (enumerPar.hasMoreElements()) {
+//            String param = enumerPar.nextElement();
+//            logger.info(param);
+//            logger.info(request.getParameter(param));
+//        }
+//
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()))) {
+//            String string = reader.readLine();
+//            while (string != null) {
+//                logger.info(string + " зашли");
+//                string = reader.readLine();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        Enumeration<String> enumer = request.getHeaderNames();
+//        while (enumer.hasMoreElements()) {
+//            String header = enumer.nextElement();
+//            logger.info(header);
+//            logger.info(request.getHeader(header));
+//        }
         return "index";
     }
 }
