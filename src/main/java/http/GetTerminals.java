0c408afd8e00b7339259455492ef219a70fc7c1b
admin <onl.ont@mail.ru>
2018-07-11 00:00:51 +0300

    Merge branch 'master' of https://github.com/SergeyI88/serevir
    # Conflicts:
    #	src/main/java/validators/FileHandler.java

--- src/main/java/http/GetTerminals.java
+++ src/main/java/http/GetTerminals.java
@@ -3,11 +3,12 @@ package http;
 import http.entity.Terminal;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 
 import java.util.List;
 
 public interface GetTerminals {
 
     @GET("https://api.evotor.ru/api/v1/inventories/devices/search")
-    Call<List<Terminal>> get(String token);
+    Call<List<Terminal>> get(@Header("X-Authorization") String token);
 }
