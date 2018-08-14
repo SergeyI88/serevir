4def9d692fd8f81ca6a18a0ae8b906705624933b
admin <onl.ont@mail.ru>
2018-08-14 10:33:02 +0300

    Merge branch 'master' of https://github.com/SergeyI88/serevir
    # Conflicts:
    #	src/main/java/validators/FileHandler.java

--- src/main/java/filter/SubscriptionFilter.java
+++ src/main/java/filter/SubscriptionFilter.java
@@ -1,41 +1,41 @@
-package filter;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-import org.springframework.web.filter.GenericFilterBean;
-import service.ClientService;
-
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import java.io.IOException;
-
-@WebFilter("/")
-public class SubscriptionFilter implements Filter {
-
-    private ClientService clientService;
-
-    public void init(FilterConfig cfg) {
-        ApplicationContext ctx = WebApplicationContextUtils
-                .getRequiredWebApplicationContext(cfg.getServletContext());
-        this.clientService = ctx.getBean(ClientService.class);
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println(clientService);
-        System.out.println("in filter " + request.getParameter("token") );
-        if (clientService.getSubscription(request.getParameter("token"))) {
-            chain.doFilter(request, response);
-        } else {
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
-            response.getWriter().write("У вас не продлена подписка");
-        }
-    }
-
-    @Override
-    public void destroy() {
-    }
-}
+//package filter;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.context.support.WebApplicationContextUtils;
+//import org.springframework.web.filter.GenericFilterBean;
+//import service.ClientService;
+//
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebFilter;
+//import java.io.IOException;
+//
+//@WebFilter("/")
+//public class SubscriptionFilter implements Filter {
+//
+//    private ClientService clientService;
+//
+//    public void init(FilterConfig cfg) {
+//        ApplicationContext ctx = WebApplicationContextUtils
+//                .getRequiredWebApplicationContext(cfg.getServletContext());
+//        this.clientService = ctx.getBean(ClientService.class);
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        System.out.println(clientService);
+//        System.out.println("in filter " + request.getParameter("token") );
+//        if (clientService.getSubscription(request.getParameter("token"))) {
+//            chain.doFilter(request, response);
+//        } else {
+//            response.setContentType("application/json");
+//            response.setCharacterEncoding("UTF-8");
+//            response.getWriter().write("У вас не продлена подписка");
+//        }
+//    }
+//
+//    @Override
+//    public void destroy() {
+//    }
+//}
