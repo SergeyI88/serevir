73c8384c5773a96229f8c15dad81cafbae7b69b9
русский <alexey.shalnoy@yandex.ru>
2018-10-03 21:27:15 +0300

    123

--- src/main/java/consts/EnumFields.java
+++ src/main/java/consts/EnumFields.java
@@ -2,25 +2,25 @@ package consts;
 
 public enum EnumFields {
     NAME("имя", null, true, null) //[ 1 .. 100 ] characters
-    , GROUP("группа", null, true, null)
-    , TYPE("тип", new String[]{"alcohol_not_marked", "normal", "alcohol_marked", "service"}, true, null)
-    , QUANTITY("количество", null, true, true)
+    , GROUP("группа", null, false, null)
+    , TYPE("тип", new String[]{"alcohol_not_marked", "normal", "alcohol_marked", "service"}, false, null)
+    , QUANTITY("количество", null, false, true)
     , MEASURE_NAME("название меры"
             , new String[]{"", "шт", "кг", "л", "м", "км", "м2", "м3", "компл", "упак", "ед", "дроб", "штука"}
-            , true, true)
-    , CODE("код", null, true, null) //[ 0 .. 10 ] characters
-    , BAR_CODES("штрих-коды", null, true, true)
+            , false, true)
+    , CODE("код", null, false, null) //[ 0 .. 10 ] characters
+    , BAR_CODES("штрих-коды", null, false, true)
     , PRICE("цена", null, true, true) //[ 0 .. 9999999.99 ] double
     , COST_PRICE("цена закупки", null, false, true) //[ 0 .. 9999999.99 ] double
-    , TAX("налог", new String[]{"NO_VAT", "VAT_10", "VAT_18", "VAT_0", "VAT_18_118", "VAT_10_110"}, true, true)
-    , ALLOW_TO_SELL("разрешено к продаже", null, true, true)
-    , DESCRIPTION("описание", null, true, true)
+    , TAX("налог", new String[]{"NO_VAT", "VAT_10", "VAT_18", "VAT_0", "VAT_18_118", "VAT_10_110"}, false, true)
+    , ALLOW_TO_SELL("разрешено к продаже", null, false, true)
+    , DESCRIPTION("описание", null, false, true)
     , ARTICLE_NUMBER("артикул", null, false, true) //[ 0 .. 20 ] characters
     , PARENT_UUID("код группы", null, false, false)
     , ALCOHOL_BY_VOLUME("объем алкогольной тары", null, false, true) //[ 0 .. 99.999 ] double
     , ALCOHOL_PRODUCT_KIND_CODE("код алкоголя", null, false, true) //от 1 до 999. int
     , TARE_VOLUME("объем тары", null, false, true)//[ 0 .. 999.999 ] double
-    , UUID("uuid", null, true, false)
+    , UUID("uuid", null, false, false)
     , ALCO_CODES("алко-коды", null, false, true);
 
     public String name;
--- src/main/java/validators/FileHandler2.java
+++ src/main/java/validators/FileHandler2.java
@@ -1,16 +1,17 @@
 package validators;
 
+import consts.EnumFields;
 import http.entity.Good;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import service.ShopService;
 import utils.MapperToEnumField;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class FileHandler2 {
     @Autowired
@@ -19,8 +20,23 @@ public class FileHandler2 {
     private MapperToEnumField mapperToEnumField;
 
 
-    public HashMap<String, List> getResult(Workbook workbook, String storeUuid, String auth) {
+    public HashMap<String, List> getResult(Workbook workbook, String storeUuid) {
+        HashMap map = new HashMap();
         Sheet sheet = workbook.getSheetAt(0);
+        Row row = sheet.getRow(0);
+        Set<Cell> firstCells = getFirstRowWithCellsName(row);
+        List<String> listErrors = new ArrayList<>();
+        requiredColumnsIsNotPresented(firstCells, listErrors);
+        if (!listErrors.isEmpty()) {
+            map.put("errors", listErrors);
+            return map;
+        }
+
+
+
+
+
+
         List<Good> listGood = new ArrayList<>();
         Queue<String> defaultSequence = mapperToEnumField.SEQUENCE;
         sheet.removeRow(sheet.getRow(0));
@@ -30,4 +46,49 @@ public class FileHandler2 {
 //        sheet.getLastRowNum()
         return new HashMap<>();
     }
+
+    /**
+     * Обязательные колонки не представлены
+     */
+    private void requiredColumnsIsNotPresented(Set<Cell> set, List<String> listErrors) {
+        boolean match = false;
+        for (EnumFields e : Arrays.stream(EnumFields.values()).filter(e -> e.isRequired).collect(Collectors.toList())) {
+            for (Cell c : set) {
+                if (e.name.toLowerCase().equals(c.toString().toLowerCase().trim())) {
+                    match = true;
+                    break;
+                }
+            }
+            if (!match) {
+                listErrors.add("Колонка: " + e.name + " отсутсвует");
+            }
+            match = false;
+        }
+    }
+    private List<Good> createListGood() {
+        List<Good> goods = new ArrayList<>();
+
+        return goods;
+    }
+    /**
+    * Добавляем в SET значение всех клеток. Даже null.
+    * capaciry это значение на которое мы увеличиваем каждый раз при клетке не null.
+    * Нужно чтобы учеть случай,когда между столбцами со значением есть пустые столбцы.
+    * Использовать только для первой строки(строки с названием колонок)
+    */
+    private Set<Cell> getFirstRowWithCellsName(Row r) {
+        Set<Cell> columns = new HashSet<>();
+        Cell c;
+        short capacity = 20;
+        for (int i = 0; i < capacity; i++) {
+            c = r.getCell(i, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+            columns.add(c);
+            if (c != null) {
+                capacity = (short) (capacity + i);
+            }
+        }
+        columns.removeIf(Objects::isNull);
+        return columns;
+    }
+
 }
